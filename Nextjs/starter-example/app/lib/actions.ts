2a1041627a4df08ac1f1ce02945c8eb976fffce7
N-Otsubo <otsubo@morijyobi.ac.jp>
2023-11-02 12:08:12 +0900

    update

--- Nextjs/starter-example/app/lib/actions.ts
+++ Nextjs/starter-example/app/lib/actions.ts
@@ -11,7 +11,7 @@ const InvoiceSchema = z.object({
         invalid_type_error: 'Please select a customer.',
     }),
     amount: z.coerce.number().gt(0, { message: 'Please enter an amount greater than $0.' }),
-    status: z.enum(['pending', 'paid'] {
+    status: z.enum(['pending', 'paid'], {
         invalid_type_error: 'Please select an invoice status.'
     }),
     date: z.string()
